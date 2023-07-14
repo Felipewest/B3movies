10b508d3d1e437c9567a95d0555f5666941c7b93
RIO-NETWORK <139229666+RIO-NETWORK@users.noreply.github.com>
2023-07-14 21:57:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,7 +54,7 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("https://telegra.ph/file/643991a87beae33a6693a.jpg") 
+        m=await message.reply_sticker("https://telegra.ph/file/1cd9480fe4579b81212b3.jpg") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
