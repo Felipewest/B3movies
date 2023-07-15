a7ecb9154fd4100d2163506db27ae7735b2fda47
RIO-NETWORK <139229666+RIO-NETWORK@users.noreply.github.com>
2023-07-15 11:43:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,7 +54,7 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("https://t.me/Mhiigrrfhikjnbbbvcghnnb") 
+        m=await message.reply_sticker("https://t.me/howtodownloadmoviestamilhd/68") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
