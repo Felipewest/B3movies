603cef86a670d6832f91fcf9d8140ba2cc4c4b52
RIO-NETWORK <139229666+RIO-NETWORK@users.noreply.github.com>
2023-07-15 13:23:56 +0530

    Update yt_dl.py

--- plugins/Extramods/yt_dl.py
+++ plugins/Extramods/yt_dl.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 from pyrogram import filters, Client
 from pyrogram.types import Message
 
-from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 
