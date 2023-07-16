9134d21560967a7a1c3a3473036a0b5e7fb87cd2
nawab0018 <135320636+nawab0018@users.noreply.github.com>
2023-07-16 22:16:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,7 +54,7 @@ async def start(client, message):
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("https://telegra.ph/file/fb2415478cf4f9bdcfb2b.jpg") 
+        m=await message.reply_sticker("https://graph.org/file/c449975691597e1792ff2.jpg") 
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
