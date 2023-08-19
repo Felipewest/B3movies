f343b12d54936083c10aaaf1a5d3a22b7d40768a
technical_aks <105633032+technicalaks@users.noreply.github.com>
2023-08-19 09:23:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,6 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 #join req accept
 CHAT_ID = [int(app_chat_id) if id_pattern.search(app_chat_id) else app_chat_id for app_chat_id in environ.get('CHAT_ID', '0').split()]
-TEXT = environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour request has been approved")
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 # Bot settings
